f7e562692caed98fbd85c2e231ce0fbcfa4e4306
abhisek7154 <abhiperl1000@gmail.com>
2025-09-06 19:28:46 +0530

    Added signup route with JWT + prisma + bcrypt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/extension'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import {sign , verify} from 'hono/jwt'
+import bcrypt from 'bcryptjs'
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
 }>()
 
@@ -12,13 +15,38 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/user/signup' , (c) => {
+app.post('/api/v1/user/signup' , async (c) => {
+try{
   const prisma = new PrismaClient({
-    DatasourceUrl: c.env.DATABASE_URL,
+    datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
-  return c.text('This is signup route')
-
   
+  const body = await c.req.json();
+  const hashedPassword  = await bcrypt.hash(body.password , 10)
+
+  const user =await prisma.user.create({
+    data: {
+      email: body.email,
+      password: hashedPassword
+    }
+  });
+
+  // JWT payload with expary
+
+  const payload = {
+    id: user.id,
+    email: user.email,
+    exp: Math.floor(Date.now() / 1000) + 60 * 60 , // 1 hour
+  }
+
+  const jwt = await sign(payload , c.env.JWT_SECRET);
+  return c.json({jwt})
+
+} catch(e){
+  c.status(403);
+  return c.json({ error: "error while signing up" });
+}
+
 })
 
 app.post('/api/v1/user/signin' , (c) => {
@@ -30,7 +58,7 @@ app.post('/api/v1/blog' , (c) => {
 })
 
 app.put('/api/v1/blog' , (c) => {
-  return c.text('Thsi is a Blog put route')
+  return c.text('This is a Blog put route')
 })
 
 app.get('/api/v1/blog/:id', (c) => {
