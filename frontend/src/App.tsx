d2a7e894e26028b57b4aa06cb0fabb8dd11d5b45
abhisek7154 <abhiperl1000@gmail.com>
2025-09-09 13:23:59 +0530

    Added frontend boiler plate code.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './input.css'
+
+function App() {
+  return (
+    <>
+    <div className="bg-gray-900">
+        <h1 className="text-center text-white">Medium clone</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
